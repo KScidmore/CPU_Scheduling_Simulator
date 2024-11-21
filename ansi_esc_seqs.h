6f887575a6246e6515bf03cc6ebba260d7a0ce8a
Kyle Scidmore <scidmore.kyle@gmail.com>
2024-11-20 18:09:41 -0700

    Added ansi stuff

---
+++ ansi_esc_seqs.h
@@ -0,0 +1,28 @@
+/*---------- ID HEADER -------------------------------------
+/  Author(s):   Andrew Boisvert, Kyle Scidmore
+/  Email(s):    abois526@mtroyal.ca, kscid125@mtroyal.ca
+/  File Name:   ansi_esc_seqs.h
+/
+/  Program Purpose(s):
+/    User-defined header file that includes some ascii escape 
+/    sequences for colour-coding output. 
+/---------------------------------------------------------*/
+
+#ifndef ANSI_ESC_SEQS_H
+#define ANSI_ESC_SEQS_H
+
+/* macro definitions */
+#define ANSI_RESET      "\033[0m"
+#define ANSI_BOLD       "\033[1m"
+#define ANSI_ITALIC     "\033[3m"
+#define ANSI_BLACK      "\033[30m"
+#define ANSI_RED        "\033[31m"
+#define ANSI_GREEN      "\033[32m"
+#define ANSI_YELLOW     "\033[33m"
+#define ANSI_BLUE       "\033[34m"
+#define ANSI_MAGENTA    "\033[35m"
+#define ANSI_CYAN       "\033[36m"
+#define ANSI_WHITE      "\033[37m"
+
+
+#endif
